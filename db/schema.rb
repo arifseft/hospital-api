864435e498f4e195ee5196679cb544e019adb04b
arifsefrianto <arif.sefrianto@qerja.com>
2020-09-18 15:57:14 +0700

    Create table user_tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_040011) do
+ActiveRecord::Schema.define(version: 2020_09_18_081705) do
+
+  create_table "user_tokens", force: :cascade do |t|
+    t.string "token", null: false
+    t.integer "expires_in", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_user_tokens_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
